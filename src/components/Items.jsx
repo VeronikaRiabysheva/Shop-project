12eb628805da3a31e8f7b75a0492eb6ec837d70a
VeronikaRiabysheva <ryabysheva01@inbox.ru>
2024-12-06 01:04:16 +0200

    shop-project

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Items(props) {
+  // const [itemCount, setItemCount] = useState(0);
+
+  // const increaseCount = () => {
+  //   setItemCount(itemCount + 1);
+  // };
   return (
     <div className="item">
       <img
@@ -15,14 +20,16 @@ export default function Items(props) {
       <p className="main-price">
         <b>${props.price}</b>
       </p>
-      <p
+      {/* <p
         className="add-to-cart"
         onClick={() => {
           props.onAdd(props);
+          increaseCount();
         }}
       >
         +
-      </p>
+      </p> */}
+      <p className="item-count">{itemCount}</p>
     </div>
   );
 }
